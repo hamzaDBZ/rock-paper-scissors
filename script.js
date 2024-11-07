94a2a44770efa44b3cef09030dc8af4d62689cf5
Hamza Rezzouq <razzoukhamza184@gmail.com>
2024-11-07 23:29:52 +0100

    Add UI to rock paper scissors project Use DOM methods instead of console.log Display all the results on the page instead of the console.

--- script.js
+++ script.js
@@ -15,37 +15,15 @@ function getComputerChoice() {
     }
 }
 
-// function getHumanChoice() {
-//     let message = "Please Enter 1 to 3 To Decide Your Choice\n" + 
-//         "[1] Rock\n" + "[2] Paper\n" + "[3] Scissors\n"
-//     let HumanChoice = +prompt(message);
-//     switch (HumanChoice) {
-//         case 1:
-//             return "rock"
-//             break;
-        
-//         case 2:
-//             return "paper"
-//             break;
-        
-//         case 3:
-//             return "scissors"
-//             break;
-//     }
-// }
-
-function getHumanChoice() {
-    let HumanChoice = prompt("Please Enter Your Choice");
-    return HumanChoice
-}
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
     let round = 0;
-    function playRound() {
+    let gameWinner;
+    function playRound(playerSelection) {
+        if (round > 5) return;
         round++;
-        let humanChoice = getHumanChoice();
+        let humanChoice = playerSelection;
         let computerChoice = getComputerChoice();
         humanChoice = humanChoice.toLowerCase()
         let roundWinner;
@@ -74,28 +52,54 @@ function playGame() {
             computerScore++;
             roundWinner = `You lose! ${computerChoice} beats ${humanChoice}`
         }
-        console.log(`\n\nRound          : ${round}\n`)
-        console.log(`Computer Choice: ${computerChoice}\n`)
-        console.log(`Your Choice    : ${humanChoice}\n`)
-        console.log(`Round Winner   : ${roundWinner}\n`)
-        console.log(`\nYour Score = ${humanScore}           Computer Score = ${computerScore}\n`)
+
+        const result = document.querySelector(".result");
+
+        if (round <= 5) {
+            if (computerScore > humanScore)
+            gameWinner = "The Computer Wins The Game"
+            else if (computerScore < humanScore)
+            gameWinner = "You win The Game"
+            else
+            gameWinner = "It's a DRAW!"
+        }
+
+        let domRound = document.createElement('div');
+        domRound.innerHTML = `<br><br>Round          : ${round}<br><br>`;
+
+        let domComputerChoice = document.createElement('div');
+        domComputerChoice.innerHTML = `Computer Choice: ${computerChoice}`;
+
+        let domYourChoice = document.createElement('div');
+        domYourChoice.innerHTML = `Your Choice    : ${humanChoice}`;
+
+        let domRoundWinner = document.createElement('div');
+        domRoundWinner.innerHTML = `Round Winner   : ${roundWinner}`;
+
+        let domYourScore = document.createElement('div');
+        domYourScore.innerHTML = `<br>Your Score = ${humanScore} <br> Computer Score = ${computerScore}<br>`;
+
+        result.appendChild(domRound);
+        result.appendChild(domComputerChoice);
+        result.appendChild(domYourChoice);
+        result.appendChild(domRoundWinner);
+        result.appendChild(domYourScore);
+
+        if (round === 5) {
+            let domGameWinner = document.createElement('div');
+            domGameWinner.innerHTML = `<br><br>${gameWinner}`;
+            result.appendChild(domGameWinner);
+            round++;
+        }
     }
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
 
-    let gameWinner;
-    if (computerScore > humanScore)
-        gameWinner = "The Computer Wins The Game"
-    else if (computerScore < humanScore)
-        gameWinner = "You win The Game"
-    else
-        gameWinner = "It's a DRAW!"
-    
-    console.log(`\n\n${gameWinner}`)
+    rock.addEventListener('click', () => playRound(rock.textContent));
+    paper.addEventListener('click', () => playRound(paper.textContent));
+    scissors.addEventListener('click', () => playRound(scissors.textContent));
 }
 
 playGame();
